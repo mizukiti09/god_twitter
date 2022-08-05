b0fc5fbb19d51fbf7ce1ddd87c72113cbbe2b5f8
mizukiti09 <gorigoritikitiki@gmail.com>
2022-08-06 02:04:47 +0900

    とりあえずcommit

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,6 +1,34 @@
 @section('title', 'God Twitter / TOP')@extends('app') @section('content')
 @include('parts.hero')
-<div class="p-home c-section">
+<div class="p-home__article">
+    <div class="u-inner">
+        <h1>
+            <i class="fab fa-twitter-square"></i>
+            Twitterの自動システム
+        </h1>
+
+        <div class="p-home__contents">
+            <p>以下のシステムをご利用頂けます。</p>
+            <ul class="p-home__ul">
+                <li>自身が選んだ他アカウントのフォロワーを対象にした自動フォロー機能(Keywordの指定も可)</li>
+                <li>自動フォローした中から15日間ツイートがない非アカウントや、1週間フォローが返ってこないアカウントを自動アンフォロー</li>
+                <li>登録した特定のKeywordを基に自動いいね機能</li>
+                <li>登録した特定のKeyword、日時を基に自動ツイート機能</li>
+            </ul>
+
+            <ul class="p-home__list">
+                <li class="p-home__list__img">
+                    <img src="{{ asset('images/follow.png') }}" alt="">
+                </li>
+                <li class="p-home__list__img">
+                    <img src="{{ asset('images/follow.png') }}" alt="">
+                </li>
+
+            </ul>
+        </div>
+    </div>
+</div>
+{{-- <div class="p-home c-section">
     <div class="p-home__wave">
         <p class="p-home__title">コンテンツが入ります。</p>
         <ul class="p-home__list">
@@ -36,5 +64,5 @@
             </li>
         </ul>
         <canvas id="waveCanvas3"></canvas>
-    </div>  
+    </div> --}}
 @endsection
--- resources/views/parts/hero.blade.php
+++ resources/views/parts/hero.blade.php
@@ -1,3 +1,5 @@
-<div class="l-hero">
-    <p class="l-hero__title">神ったー</p>
+<div class="l-hero__container">
+    <div class="l-hero">
+        <p class="l-hero__title">神ったー</p>
+    </div>
 </div>
